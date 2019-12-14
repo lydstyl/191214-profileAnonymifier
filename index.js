f753c438c983b4aa04c7d9ef93b39f7f2222449e
Gabriel Brun <lydstyl@gmail.com>
2019-12-14 15:08:33 +0100

    second

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
 const path = require('path');
+const dotenv = require('dotenv');
 const scrapedin = require('scrapedin');
 const PizZip = require('pizzip');
 const Docxtemplater = require('docxtemplater');
 
+const jsonProfile = `./profiles/${process.env.PROFIL}.json`;
+
 function scrapeProfileAndMakeResume() {
-  if (fs.existsSync('./profile.json')) {
+  if (fs.existsSync(jsonProfile)) {
     writeDocx();
   } else {
     scrapedin({
@@ -13,45 +16,43 @@ function scrapeProfileAndMakeResume() {
       password: process.env.PASSWORD
     })
       .then(profileScraper =>
-        // profileScraper('https://www.linkedin.com/in/gabrielbrun/')
-        profileScraper('https://www.linkedin.com/in/elodiebattaire/')
+        profileScraper(`https://www.linkedin.com/in/${process.env.PROFIL}/`)
       )
       .then(profile => {
-        fs.writeFile(
-          'profile.json',
-          JSON.stringify(profile, null, 2),
-          writeDocx
-        );
+        fs.writeFile(jsonProfile, JSON.stringify(profile, null, 2), writeDocx);
       });
   }
 }
 
 function writeDocx() {
-  const profile = require('./profile.json');
-  console.log(profile);
+  const profile = require(jsonProfile);
 
-  //Load the docx file as a binary
   const content = fs.readFileSync(
     path.resolve(__dirname, 'cvTemplate.docx'),
     'binary'
   );
-
   const zip = new PizZip(content);
-
   const doc = new Docxtemplater();
   doc.loadZip(zip);
 
-  //set the templateVariables
+  console.log(profile.educations);
+
   doc.setData({
-    name: profile.profileAlternative.name,
-    headline: profile.profileAlternative.headline
+    name: profile.profileAlternative.name || 'CV',
+    headline: profile.profileAlternative.headline,
+    description: profile.profile.summary,
+    skills: profile.skills,
+    experiences: profile.positions.map(xp => ({
+      begin: xp.date1 ? xp.date1 : '',
+      job: xp.title,
+      company: xp.companyName
+    })),
+    educations: profile.educations
   });
 
   try {
-    // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
     doc.render();
   } catch (error) {
-    // The error thrown here contains additional information when logged with JSON.stringify (it contains a properties object).
     var e = {
       message: error.message,
       name: error.name,
@@ -66,17 +67,19 @@ function writeDocx() {
         })
         .join('\n');
       console.log('errorMessages', errorMessages);
-      // errorMessages is a humanly readable message looking like this :
-      // 'The tag beginning with "foobar" is unopened'
     }
     throw error;
   }
 
   const buf = doc.getZip().generate({ type: 'nodebuffer' });
 
-  // buf is a nodejs buffer, you can either write it to a file or do anything else with it.
-  fs.writeFileSync(path.resolve(__dirname, 'cv.docx'), buf);
+  fs.writeFileSync(
+    path.resolve(__dirname, `./profiles/${process.env.PROFIL}.docx`),
+    buf
+  );
   console.log('finish');
 }
+
+dotenv.config();
 
 scrapeProfileAndMakeResume();
